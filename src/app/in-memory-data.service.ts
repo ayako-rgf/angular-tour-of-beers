e1593d2ca384d07adb419d0d3b6da3b4969ec1d9
Ayako Sainoki <ayako@r.recruit.co.jp>
2018-06-06 15:03:59 +0800

    photo: change photos and beers order

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,12 +3,12 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const beers = [
-            { id: 11, name: 'Tiger Lager Beer', country: 'Singapore' },
             { id: 12, name: 'Heineken Lager Beer', country: 'Singapore' },
+            { id: 11, name: 'Tiger Lager Beer', country: 'Singapore' },
             { id: 13, name: 'Chang Beer Premium Draft Beer', country: 'Thailand' },
-            { id: 14, name: 'Corona Extra Beer', country: 'Mexico' },
-            { id: 15, name: 'Carlsberg Lager Beer', country: 'Malaysia' },
             { id: 16, name: 'Sapporo Premium Draft Beer', country: 'Vietnam' },
+            { id: 15, name: 'Carlsberg Lager Beer', country: 'Malaysia' },
+            { id: 14, name: 'Corona Extra Beer', country: 'Mexico' },
             { id: 17, name: 'Anchor Smooth Pilsener Beer', country: 'Singapore' },
             { id: 18, name: 'Kirin Ichiban Lager Beer', country: 'Japan' },
             { id: 19, name: 'Guinness Draught', country: 'Ireland' },
